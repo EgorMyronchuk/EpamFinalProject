891da290e7370bfd4abe55dc0043f09710ef0953
egormyronchuk <egormyronchuk@gmail.com>
2026-02-07 22:37:42 +0200

    add test

---
+++ src/test/java/com/epam/rd/autocode/spring/project/controller/CustomErrorControllerTest.java
@@ -0,0 +1,82 @@
+package com.epam.rd.autocode.spring.project.controller;
+
+import com.epam.rd.autocode.spring.project.repo.UserRepository;
+import com.epam.rd.autocode.spring.project.service.CartService;
+import com.epam.rd.autocode.spring.project.service.ClientService;
+import com.epam.rd.autocode.spring.project.service.EmployeeService;
+import com.epam.rd.autocode.spring.project.service.authService.JwtService;
+import jakarta.servlet.RequestDispatcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomErrorController.class)
+class CustomErrorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private CartService cartService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private ClientService clientService;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    @WithMockUser
+    void testHandleError404() throws Exception {
+        mockMvc.perform(get("/error")
+                        .requestAttr(RequestDispatcher.ERROR_STATUS_CODE, 404))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error-page"))
+                .andExpect(model().attribute("statusCode", 404))
+                .andExpect(model().attribute("message", "Сторінку не знайдено"));
+    }
+
+    @Test
+    @WithMockUser
+    void testHandleError500() throws Exception {
+        mockMvc.perform(get("/error")
+                        .requestAttr(RequestDispatcher.ERROR_STATUS_CODE, 500))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error-page"))
+                .andExpect(model().attribute("statusCode", 500))
+                .andExpect(model().attribute("message", "Помилка сервера. Ми вже працюємо над цим"));
+    }
+
+    @Test
+    @WithMockUser
+    void testHandleErrorOtherStatus() throws Exception {
+        mockMvc.perform(get("/error")
+                        .requestAttr(RequestDispatcher.ERROR_STATUS_CODE, 403))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error-page"))
+                .andExpect(model().attribute("statusCode", 403))
+                .andExpect(model().attribute("message", "Щось пішло не так"));
+    }
+
+    @Test
+    @WithMockUser
+    void testHandleErrorWithoutStatus() throws Exception {
+        // Перевірка випадку, коли атрибут помилки відсутній
+        mockMvc.perform(get("/error"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error-page"))
+                .andExpect(model().attributeDoesNotExist("statusCode"));
+    }
+}
